fb50d9db240055f0b63198a9e2d13a4a3740c3c4
ballasankarreddy <ballasankarreddy@gmail.com>
2019-01-20 17:21:08 +0530

    ex.js file added

---
+++ Test/ex.js
@@ -0,0 +1,17 @@
+  
+var app=angular.module("myApp",[]);
+
+app.controller("loginController",function($scope){
+	$scope.message="I'm from LogingController";
+	$scope.firstName          ="Sankar";
+	$scope.lastName           ="Reddy";
+	$scope.email			  ="ballasankarReddy@gmail.com";
+	$scope.gender 			  ="Mail";
+	$scope.phoneNumber        ="9080682900";
+	$scope.password           ="siri";
+	$scope.conformPassword   ="siri";
+	
+});
+
+
+itprooffncr@hcl.com
---
+++ TicktbookingApplication/ex.js
@@ -0,0 +1,18 @@
+  
+var app=angular.module("myApp",[]);
+
+app.controller("loginController",function($scope){
+	$scope.message="I'm from LogingController";
+	$scope.firstName          ="Sankar";
+	$scope.lastName           ="Reddy";
+	$scope.email			  ="ballasankarReddy@gmail.com";
+	$scope.gender 			  ="Mail";
+	$scope.phoneNumber        ="9080682900";
+	$scope.password           ="siri";
+	$scope.conformPassword   ="siri";
+	
+});
+
+
+itprooffncr@hcl.com
+Siri@4reddy
